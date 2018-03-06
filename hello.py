c7dc1ac71a24898685dc0201a0ae52a55de8dadb
Patrick Deegan <pat667deegan@gmail.com>
2018-03-06 13:56:42 -0500

    Cheat sheet added

---
+++ hello.py
@@ -0,0 +1,33 @@
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:54:40) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+======================= RESTART: C:\Pat_Rick\hello.py =======================
+hello world
+4
+2
+69
+4.0
+9
+We waste time
+while time slowly kills us
+None
+>>> 
+======================= RESTART: C:\Pat_Rick\hello.py =======================
+hello world
+4
+2
+69
+4.0
+9
+>>> 
+======================= RESTART: C:\Pat_Rick\hello.py =======================
+hello world
+4
+2
+69
+4.0
+9
+We waste time
+while time slowly kills us
+-OAA
+>>> 
